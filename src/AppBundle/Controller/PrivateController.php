a94522086cbc271171ec2f91beffd5b14beae301
Juan <evovang@gmail.com>
2016-09-07 09:29:59 +0000

    74. Activar la acción de publicar trayecto

--- src/AppBundle/Controller/PrivateController.php
+++ src/AppBundle/Controller/PrivateController.php
@@ -23,7 +23,26 @@ class PrivateController extends Controller
     */
    public function publicarTrayectoAction(Request $request)
    {
-       // TODO: Hacer
+      // TODO: Hacer
+      $nuevoTrayecto = new Trayecto();
+      $nuevoTrayecto->setOrigen(($request->get('origen')));
+      $nuevoTrayecto->setDestino($request->get('destino'));
+      $nuevoTrayecto->setCalle($request->get('calle'));
+      $fechaDateTime = new \DateTime($request->get('fechaDeViaje'));
+      $nuevoTrayecto->setFechaDeViaje($fechaDateTime);
+      $horaDateTime = new \DateTime($request->get('horaDeViaje'));
+      $nuevoTrayecto->setHoraDeViaje($horaDateTime);
+      $nuevoTrayecto->setPrecio($request->get('precio'));
+      $nuevoTrayecto->setDescripcion($request->get('descripcion'));
+      $nuevoTrayecto->setPlazas($request->get('plazas'));
+      $usuarioLogueado = $this->getUser();
+      $nuevoTrayecto->setConductor($usuarioLogueado);
+      
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($nuevoTrayecto);
+      $entityManager->flush();
+      
+      return $this->redirect($this->generateUrl('public_home'));
    }
 }
 
