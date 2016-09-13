4c52916300f9fb29d94ab5a76c9a61bc5165f72a
Juan <evovang@gmail.com>
2016-09-13 05:46:07 +0000

    Habilitar pantalla list

--- src/AppBundle/Controller/PublicController.php
+++ src/AppBundle/Controller/PublicController.php
@@ -20,5 +20,41 @@ class PublicController extends Controller
      */
     public function listAction(Request $request)
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $repositorioTrayecto = $entityManager->getRepository("AppBundle:Trayecto");
+        $trayectos = $repositorioTrayecto->findAll();
+        
+        // Si no se indica un filtro para la fecha, se muestran todos los trayectos
+    if ($request->get('posted') != "" || $request->get('country') != "") {
+        $parameters=array();
+        $queryBuilder = $repositorioTrayecto
+            ->createQueryBuilder('trayecto');
+        if ($_GET["country"]!="") {
+            $queryBuilder->where('trayecto.origen = :country')
+            ->orWhere('trayecto.destino = :country');
+            $parameters['country']=$request->get('country');
+        }   
+        if($request->get('posted') != ""){
+            if ($request->get('country')!="") {
+                $queryBuilder->andWhere('fecha.createdAt > :posted');
+            } 
+            else {
+                $queryBuilder->where('fecha.createdAt > :posted');
+            } 
+            $date=new DateTime();
+            $date->modify('-'.$request->get('posted').' day');
+            $parameters['posted']=$date;
+        }
+        $trayectosFiltrados =$queryBuilder->setParameters($parameters)
+        ->getQuery()->execute();
+    } else {
+        $trayectosFiltrados = $trayectos;
+    }    
+    
+    return $this->render('list/index.html.twig', array(
+    'trayectos' => $trayectosFiltrados,
+    'posted' => $request->get('posted'),
+    'country' => $request->get('country')
+        ));
     }
 }
